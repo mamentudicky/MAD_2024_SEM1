dd9bf749bb59a022b76e37fa770e251c3c64e7e2
mamentudicky <dickymamentu@gmail.com>
2024-10-22 22:18:36 +0800

    AWESOMEPROJECT

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SplashScreen from './src/pages/SplashScreen';
+import SignIn from './src/pages/SignIn';
+
+const App = () => {
+  return <SignIn />;
+};
+
+export default App;
---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,35 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {Header, TextInput} from '../../components/molecules';
+import {Button, Gap} from '../../components/atoms';
+
+const SignIn = () => {
+  return (
+    <View style={styles.container}>
+      <Header text="Sign In" />
+      <View style={styles.contentWrapper}>
+        <TextInput label="Email Addres" placeholder="Type your email address" />
+        <Gap height={16} />
+        <TextInput label="Password" placeholder="Type your password" />
+        <Gap height={24} />
+        <Button text="Sign In" />
+        <Gap height={12} />
+        <Button text="Create New Account" color="#8D92A3" textColor="#FFFFFF" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentWrapper: {
+    marginTop: 24,
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 24,
+    paddingTop: 25,
+  },
+});
+export default SignIn;
